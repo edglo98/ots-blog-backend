664738a94a4d8577b2f41d43ca64ece81341cc2a
Edgar Lopez Lopez <edgarlopezlopez@Edgars-MacBook-Air.local>
2022-07-15 01:26:54 -0500

    feature algolia index add for post

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,15 @@
 module.exports = ({ env }) => ({
+  search: {
+    enabled: true,
+    config: {
+      provider: 'algolia',
+      providerOptions: {
+        apiKey: env('ALGOLIA_PROVIDER_ADMIN_API_KEY'),
+        applicationId: env('ALGOLIA_PROVIDER_APPLICATION_ID'),
+      },
+      contentTypes: [{ name: 'api::post.post' }],
+    },
+  },
   // ...
   email: {
     config: {
